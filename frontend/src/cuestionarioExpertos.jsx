9b824b6bb2b7ab808bd9edeecd442733aeb0f58b
Sebastian Lopez Garrido <logasebas0407@gmail.com>
2026-05-21 14:45:35 -0600

    Actualizacion import navigate cuestionario Experto y Pymes

--- frontend/src/cuestionarioExpertos.jsx
+++ frontend/src/cuestionarioExpertos.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function CuestionarioExpertos() {
   const [error, setError] = useState("");
