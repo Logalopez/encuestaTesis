40a9f8694a83f28f11b745b65821b0ae44250337
Sebastian Lopez Garrido <logasebas0407@gmail.com>
2026-05-21 14:50:39 -0600

    Actualizacion import 2do intento navigate cuestionario Experto y Pymes

--- frontend/src/cuestionarioExpertos.jsx
+++ frontend/src/cuestionarioExpertos.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function CuestionarioExpertos() {
   const [error, setError] = useState("");
--- frontend/src/cuestionarioPymes.jsx
+++ frontend/src/cuestionarioPymes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function CuestionarioPymes() {
   const [formData, setFormData] = useState({
